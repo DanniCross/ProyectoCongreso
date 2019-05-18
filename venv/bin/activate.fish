9afd3ca9bdc99bf1cc8648b8663968a90382f078
Camilog699 <amraga10@gmail.com>
2019-05-18 10:25:25 -0500

    addnode

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/run/media/josec/Jose Cruz/Documentos/Pycharm Projects/ProyectoI/venv"
+set -gx VIRTUAL_ENV "/home/camilog/Descargas/ProyectosPyCharm/ProyectoCongreso/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
