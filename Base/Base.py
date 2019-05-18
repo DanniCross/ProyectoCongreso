3a3bd6ef330c97ad237b18150e1ac956a3e33a80
Jose Cruz <josedanielcrzgmail.com>
2019-05-18 15:20:13 -0500

    actual

---
+++ Base/Base.py
@@ -0,0 +1,5 @@
+class Base:
+
+    def __init__(self, posx, posy):
+        self.posx = posx
+        self.posy = posy
---
+++ Resources/Party.py
@@ -0,0 +1,7 @@
+class Party:
+
+    def __init__(self, id, name, leader, color):
+        self.id = id
+        self.name = name
+        self.leader = leader
+        self.color = color
