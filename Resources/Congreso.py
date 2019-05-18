7048599684161a7ba584b37b07381268295f05ef
Camilog699 <amraga10@gmail.com>
2019-05-18 10:23:58 -0500

    addNode

--- Resources/Congreso.py
+++ Resources/Congreso.py
@@ -1,2 +1,20 @@
+from .Congresista import Congresista
+
 class Congreso:
-    pass
+
+    def __init__(self, nodes, root):
+        self.nodes = nodes
+        self.root = root
+
+    def add(self, ):
+        newCongresista = Congresista()
+        if self.root is None:
+            self.root = newCongresista
+        else:
+            addNode()
+
+    def addNode(self, parent):
+        newCongresista = Congresista()
+        if parent is None:
+            parent = newCongresista
+
